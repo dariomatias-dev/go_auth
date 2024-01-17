67f40b5e43189568db81f5dbe57d4c61a30b876d
dario <matiasdario75@gmail.com>
2024-01-17 08:17:07 -0300

    feat: created file for launching PostgreSQL database and code for obtaining connection to the database
    - Created docker-compose file for launching a docker container with the PostgreSQL database.
    - Created code to obtain connection with the database.

--- main.go
+++ main.go
@@ -1,14 +1,45 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
 
+	db "github.com/dariomatias-dev/go_auth/api/db/sqlc"
 	"github.com/dariomatias-dev/go_auth/api/routes"
 	"github.com/dariomatias-dev/go_auth/initialize"
 )
 
+var dbQueries *db.Queries
+
 func init() {
 	initialize.Load()
+
+	dbName := os.Getenv("DATABASE_DRIVER")
+	dbURL := fmt.Sprintf(
+		"%s://%s:%s@%s:%s/%s?sslmode=disable",
+		dbName,
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	dbcon, err := sql.Open(dbName, dbURL)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := dbcon.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	dbQueries = db.New(dbcon)
 }
 
 func main() {
