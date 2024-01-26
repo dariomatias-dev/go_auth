8b8b7e163caace6a920659f2a2868b3c240ec18b
dario <matiasdario75@gmail.com>
2024-01-26 08:31:05 -0300

    feat: finished token verification middleware
    - Finalized development of the access token verification middleware.

--- api/middlewares/verify_token_middleware.go
+++ api/middlewares/verify_token_middleware.go
@@ -1,8 +1,12 @@
 package middlewares
 
 import (
-	"github.com/gin-gonic/gin"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+
+	tokentype "github.com/dariomatias-dev/go_auth/api/enums/token_type"
 	"github.com/dariomatias-dev/go_auth/api/services"
 )
 
@@ -10,5 +14,28 @@ func VerifyToken(
 	ctx *gin.Context,
 	authService services.AuthService,
 ) {
+	token, ok := authService.GetToken(ctx)
+	if !ok {
+		return
+	}
 
+	payload, ok := authService.GetPayload(
+		ctx,
+		*token,
+	)
+	if !ok {
+		return
+	}
+
+	if mapClaims, ok := payload.Claims.(jwt.MapClaims); ok || payload.Valid {
+		if mapClaims["token_type"] != tokentype.AccessToken {
+			ctx.AbortWithStatusJSON(
+				http.StatusUnauthorized,
+				gin.H{
+					"message": "invalid token",
+				},
+			)
+			return
+		}
+	}
 }
